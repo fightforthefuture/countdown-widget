bac6da08f5a1d057e2cc721e83bc8b8a817d0230
Jeff L <jeff@rubbingalcoholic.com>
2015-01-22 10:54:57 -0800

    initial checkin

---
+++ README.md
@@ -0,0 +1,70 @@
+The Internet Countdown Widget
+-----------------------------
+#### Join the battle for net neutrality with our Internet Countdown widget!
+
+[**The FCC is voting on net neutrality rules**][1] on February 26th, 2015. But
+cable monopolies are trying to derail the process. The goal of this
+project is to allow anyone with a web site to run their own campaign to save net
+neutrality. Simply embed a couple of lines of Javascript and you're good to go!
+
+To stay up-to-date on developments, please subscribe to our
+[**developer mailing list**][1]. If you have any problems or questions regarding
+the widget, please [submit an issue][4] or [email Jeff Lyon][5].
+
+
+Installation Instructions (Banner)
+---------------------------------
+[**Click here to see the demo**][3]
+
+![Blue version (default)](https://fightforthefuture.github.io/countdown-widget/demos/widget.red.png)
+![Red version](https://fightforthefuture.github.io/countdown-widget/demos/widget.red.png)
+
+#### Basic installation:
+
+**The banner will show up wherever you put this snippet of code on your page:**
+```html
+<div><script src="//fightforthefuture.github.io/countdown-widget/widget.min.js"></script></div>
+```
+
+**Red version:**
+```html
+<script type="text/javascript">var _cd_options = { theme: 'red' };</script>
+<div><script src="//fightforthefuture.github.io/countdown-widget/widget.min.js"></script></div>
+```
+
+#### Optimizing for fast loading:
+
+You can speed up loading of your page by loading the banner asynchronously.
+Here's how.
+
+**1. Place this code at the bottom of your page, right before the closing
+/BODY tag:**
+```html
+<script type="text/javascript">
+var _cd_options = {
+    theme: 'blue', // or 'red'
+    elementId: '_cd_container'
+}
+</script>
+<script src="//fightforthefuture.github.io/countdown-widget/widget.min.js" async>
+```
+
+**2. Then, place this empty DIV anywhere on your page where you want the banner
+to appear:**
+```html
+<div id="_cd_container"></div>
+```
+
+
+Which browsers are supported
+----------------------------
+Modern browsers, and the two latest versions of Internet Explorer (10 and 11).
+
+
+[1]: https://www.battleforthenet.com/countdown
+[2]: https://fightforthefuture.github.io/countdown-widget/demos/modal.html
+[3]: https://fightforthefuture.github.io/countdown-widget/demos/banner.html
+[4]: https://github.com/fightforthefuture/countdown-widget/issues
+[5]: mailto:jeff@fightforthefuture.org
+[6]: https://github.com/fightforthefuture/countdown-widget/blob/master/widget.js
+[7]: https://fightforthefuture.github.io/countdown-widget/demos/banner.dark.html
---
+++ widget.js
@@ -0,0 +1,215 @@
+/*
+ @licstart  The following is the entire license notice for the
+    JavaScript code in this page.
+
+ Copyright (C) 2014 Center for Rights in Action
+ Copyright (C) 2014 Jeff Lyon
+
+ The JavaScript code in this page is free software: you can
+ redistribute it and/or modify it under the terms of the GNU
+ General Public License (GNU GPL) as published by the Free Software
+ Foundation, either version 3 of the License, or (at your option)
+ any later version. The code is distributed WITHOUT ANY WARRANTY;
+ without even the implied warranty of MERCHANTABILITY or FITNESS
+ FOR A PARTICULAR PURPOSE. See the GNU GPL for more details.
+
+ As additional permission under GNU GPL version 3 section 7, you
+ may distribute non-source (e.g., minimized or compacted) forms of
+ that code without the copy of the GNU GPL normally required by
+ section 4, provided you include this license notice and a URL
+ through which recipients can access the Corresponding Source.
+
+ @licend  The above is the entire license notice
+    for the JavaScript code in this page.
+*/
+
+(function(){ // :)
+
+// Default URL for animation iframe. This gets overlay'ed over your page.
+var dfurl = 'https://widget.battleforthenet.com/iframe';
+
+
+/**
+--------------------------------------------------------------------------------
+CONFIGURATION OPTIONS
+--------------------------------------------------------------------------------
+These are default configuration values for the widget. You can override any of
+these by pre-defining an object named _cd_options and setting the appropriate
+properties as desired.
+--------------------------------------------------------------------------------
+*/
+
+// The _cd_options object is created if it isn't already defined by you
+if (typeof _cd_options == "undefined")
+	_cd_options = {};
+
+// The path to the iframe that gets injected over your page
+if (typeof _cd_options.iframe_base_path == "undefined")
+	_cd_options.iframe_base_path = dfurl;
+
+// Which design to show, either "modal" or "banner" (see _cd_animations below)
+if (typeof _cd_options.animation == "undefined")
+	_cd_options.animation = 'banner';
+
+/**
+--------------------------------------------------------------------------------
+ANIMATION DEFINITIONS
+--------------------------------------------------------------------------------
+Here's where the functionality and defaults for each of the animations (either
+"modal" or "banner" to begin with). Each animation has its own options property,
+which is an object containing default behaviors for that animation. These can be
+overridden by passing the appropriately-named properties into the _cd_options
+object (above). This will get merged over the defaults when init is called.
+--------------------------------------------------------------------------------
+*/
+var _cd_animations = {
+
+	// BANNER ANIMATION
+	banner: {
+
+		// Default options: Override these with _cd_options object (see above)
+		options: {
+			modalAnimation: 'banner',
+			width: 324,
+			height: 142,
+			url: 'https://www.battleforthenet.com/',
+			theme: 'blue',
+			elementId: null
+		},
+
+		// init copies the _cd_options properties over the default options
+		init: function(options) {
+			for (var k in options) this.options[k] = options[k];
+			return this;
+		},
+
+		// what to do when the animation starts
+		start: function() {
+
+			var css = '#_cd_iframe { \
+					width: '+this.options.width+'px; \
+					height: '+this.options.height+'px; \
+				}';
+
+			_cd_util.injectCSS('_cd_iframe_css', css);
+
+			if (this.options.elementId)
+				var el = document.getElementById(this.options.elementId);
+			else if (_CD_INJECT_ELEM)
+				var el = _CD_INJECT_ELEM;
+			
+			if (!el)
+				return console.error('No element found for Countdown Widget');
+
+			var iframe = _cd_util.createIframe(el, this.options.modalAnimation);
+			_cd_util.bindIframeCommunicator(iframe, this);
+		},
+
+		// what to do when the animation stops
+		stop: function() {
+			_cd_util.destroyIframe();
+		}
+	}
+}
+
+/**
+--------------------------------------------------------------------------------
+UTILITY FUNCTIONS
+--------------------------------------------------------------------------------
+*/
+var _cd_util = {
+
+	// Inject CSS styles into the page
+	injectCSS: function(id, css)
+	{
+		var style = document.createElement('style');
+		style.type = 'text/css';
+		style.id = id;
+		if (style.styleSheet) style.styleSheet.cssText = css;
+		else style.appendChild(document.createTextNode(css));
+		document.head.appendChild(style);
+	},
+
+	// Create the iframe used to display the animation  
+	createIframe: function(el, animation) {
+		var iframe = document.createElement('iframe');
+		iframe.id = '_cd_iframe';
+		iframe.src = _cd_options.iframe_base_path + '/' + animation + '.html';
+		iframe.frameBorder = 0;
+		iframe.allowTransparency = true; 
+		iframe.style.display = 'none';
+		el.appendChild(iframe);
+		return iframe;
+	},
+
+	// Destroy the iframe used to display the animation
+	destroyIframe: function() {
+		var iframe = document.getElementById('_cd_iframe');
+		iframe.parentNode.removeChild(iframe);
+	},
+
+	// Sends / receives event messages to the iframe (IE9+)
+	// Necessary because the iframe lives on a different domain and we can't
+	// just call Javascript functions to/from it due to XSS protections.
+	bindIframeCommunicator: function(iframe, animation) {
+		var sendMessage = function(requestType, data)
+		{
+			data || (data = {});
+			data.requestType = requestType;
+			data.CD_WIDGET_MSG = true;
+			data.HOST_NAME = hostname;
+			iframe.contentWindow.postMessage(data, '*');
+		}
+
+		var method = window.addEventListener ? "addEventListener":"attachEvent";
+		var eventer = window[method];
+		var messageEvent = method == "attachEvent" ? "onmessage":"message";
+
+		var hostname = window.location.host.replace('www.', '');
+
+		eventer(messageEvent,function(e) {
+			if (!e.data || !e.data.CD_IFRAME_MSG)
+				return;
+
+			delete e.data.CD_IFRAME_MSG;
+
+			switch (e.data.requestType) {
+				case 'getAnimation':
+					iframe.style.display = 'block';
+					sendMessage('putAnimation', animation.options);
+					break;
+				case 'stop':
+					animation.stop();
+					break;
+			}
+		}, false);
+
+	}
+}
+
+/**
+--------------------------------------------------------------------------------
+MAIN FUNCTIONALITY (called once the page is ready)
+--------------------------------------------------------------------------------
+*/
+var ready = function() {
+	var animation = _cd_animations[_cd_options.animation];
+	animation.init(_cd_options).start();
+}
+
+// Wait for DOM content to load.
+var curState = document.readyState;
+if (curState=="complete" || curState=="loaded" || curState=="interactive") {
+	ready();
+} else if (document.addEventListener) {
+	document.addEventListener('DOMContentLoaded', ready, false);
+}
+
+
+})(); // :)
+
+
+var target = document.documentElement;
+while (target.childNodes.length && target.lastChild.nodeType == 1)
+    target = target.lastChild;
+var _CD_INJECT_ELEM = target.parentNode;
